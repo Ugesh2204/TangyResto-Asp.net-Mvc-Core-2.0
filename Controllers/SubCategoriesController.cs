616cf84ac971d363b14f9623df95bb10f26fb4cd
ugesh <ugesh.roopchand@hotmail.com>
2018-09-21 10:07:13 +0400

    Subcategory Added

---
+++ Controllers/SubCategoriesController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tangy.Data;
+using Tangy.Models;
+using Tangy.Views.SubCategoryViewModels;
+
+namespace Tangy.Controllers
+{
+    public class SubCategoriesController : Controller
+    {
+        //Dependency Injection
+
+        private readonly ApplicationDbContext _db;
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public SubCategoriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //Get Action Asychronous method
+        public async Task<IActionResult> Index()
+        {
+            //Eager Loading
+
+            var subCategories = _db.SubCategory.Include(s => s.Category);
+
+            return View(await subCategories.ToListAsync());
+        }
+
+
+        public IActionResult Create()
+        {
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = _db.Category.ToList(),
+                SubCategory = new SubCategory(),
+                SubcategoryList = _db.SubCategory.OrderBy(p => p.Name)
+                                    .Select(p => p.Name)
+                                    .ToList()
+            };
+
+
+            return View(model);
+        }
+
+
+
+        //POST Create
+        [HttpPost]
+        public async Task<IActionResult>Create(SubCategoryAndCategoryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var doesSubCategoryExists = _db.SubCategory.Where(s => s.Name == model.SubCategory.Name).Count();
+                var doesSubCatAndCatExists = _db.SubCategory
+                                            .Where(s => s.Name == model.SubCategory.Name 
+                                            && s.CategoryId == model.SubCategory.CategoryId).Count();
+
+
+                if(doesSubCategoryExists > 0 && model.isNew)
+                {
+                    //error
+                    StatusMessage = "Error : Sub Category Name already Exists";
+                }
+                else
+                {
+                    if(doesSubCategoryExists == 0 && !model.isNew)
+                    {
+                        //error
+                        StatusMessage = "Error : Sub Category does not exists";
+                    }
+                    else
+                    {
+                        if (doesSubCatAndCatExists > 0)
+                        {
+                            //error
+                            StatusMessage = "Error : Category and sub category combination exists";
+                        }
+                        else
+                        {
+                            _db.Add(model.SubCategory);
+                            await _db.SaveChangesAsync();
+                            return RedirectToAction(nameof(Index));
+                        }
+                        
+
+                    }
+                }
+
+
+            }
+
+            SubCategoryAndCategoryViewModel modelVM = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = _db.Category.ToList(),
+                SubCategory = model.SubCategory,
+                SubcategoryList = _db.SubCategory.OrderBy(p => p.Name)
+                                    .Select(p => p.Name)
+                                    .ToList(),
+                StatusMessage = StatusMessage
+            
+            };
+
+            return View(modelVM);
+        }
+    }
+}
---
+++ Views/SubCategoryViewModels/SubCategoryAndCategoryViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Tangy.Models;
+
+namespace Tangy.Views.SubCategoryViewModels
+{
+    //Drowp down list category
+    public class SubCategoryAndCategoryViewModel
+    {
+        public SubCategory SubCategory { get; set; }
+        public IEnumerable<Category> CategoryList { get; set; }
+
+        public List<string> SubcategoryList { get; set; }
+
+        [Display(Name = "New sub Category")]
+        public bool isNew { get; set; }
+
+        public string StatusMessage { get; set; }
+    }
+}
