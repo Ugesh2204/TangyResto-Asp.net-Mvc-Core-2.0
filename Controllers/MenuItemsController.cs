c2357a3bea9224a2957b6fc6143646a208537537
ugesh <ugesh.roopchand@hotmail.com>
2018-09-30 10:28:49 +0400

    Create and upload image done

--- Controllers/MenuItemsController.cs
+++ Controllers/MenuItemsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -9,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Tangy.Data;
 using Tangy.Models;
 using Tangy.Models.MenuItemViewModels;
+using Tangy.Utility;
 
 namespace Tangy.Controllers
 {
@@ -50,6 +52,66 @@ namespace Tangy.Controllers
         {
             return View(MenuItemVM);
         }
+
+
+        //POST:MenuItem Create
+        [HttpPost,ActionName("Create")]
+        public async Task<IActionResult> CreatePOST()
+        {
+            MenuItemVM.MenuItem.SubCategoryId = Convert.ToInt32(Request.Form["SubcategoryId"].ToString());
+            if (!ModelState.IsValid)
+            {
+                return View(MenuItemVM);
+            }
+
+            _db.MenuItem.Add(MenuItemVM.MenuItem);
+            await _db.SaveChangesAsync();
+
+
+            //save image
+            //this will retive the www root folder
+            string WebRootPath = _hostingEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+
+            var menuItemFromDb = _db.MenuItem.Find(MenuItemVM.MenuItem.Id);
+
+
+            if(files[0]!=null && files[0].Length > 0)
+            {
+                //when user uploads an image
+                var uploads = Path.Combine(WebRootPath, "images");
+                var extension = files[0]
+                                .FileName
+                                .Substring(files[0].FileName.LastIndexOf("."),
+                                files[0].FileName.Length-files[0].FileName.LastIndexOf("."));
+
+                using (var filestream = new FileStream(Path.Combine(uploads, MenuItemVM.MenuItem.Id + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(filestream);
+                }
+
+                menuItemFromDb.Image = @"\images\" + MenuItemVM.MenuItem.Id + extension;
+            }
+
+            else
+            {
+                //when user does not upload and image 
+                var uploads = Path.Combine(WebRootPath, @"images\"+ SD.DefaultFoodImage);
+                System.IO.File.Copy(uploads, WebRootPath + @"\images\" + MenuItemVM.MenuItem.Id + ".png");
+                menuItemFromDb.Image= @"\images\" + MenuItemVM.MenuItem.Id + ".png";
+            }
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+
+
+
 
         //Loading subcatogry from database 
         public JsonResult GetSubCategory(int CategoryId)
